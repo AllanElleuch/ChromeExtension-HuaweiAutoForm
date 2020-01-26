c5747c1282c31825ed8f137b992b5f60d5f4c1c2
Allan Elleuch <allan.elleuch@gmail.com>
2020-01-26 14:35:20 +0100

    add auto translation to parse all languages

--- js/apiexpress_content.js
+++ js/apiexpress_content.js
@@ -1,4 +1,4 @@
-// https://developer.chrome.com/extensions/messaging#simple 
+// https://developer.chrome.com/extensions/messaging#simple
 // function injectScript(file, node) {
 //     var th = document.getElementsByTagName(node)[0];
 //     var s = document.createElement('script');
@@ -9,113 +9,141 @@
 // injectScript(chrome.extension.getURL('js/injectScript.js'), 'body');
 
 function getElementByXpath(path) {
-    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  return document.evaluate(
+    path,
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue;
 }
 
+$(document).ready(function() {
+  if (location.href.includes("appinfo")) {
+    chrome.runtime.onMessage.addListener(function(
+      request,
+      sender,
+      sendResponse
+    ) {
+      console.log("content script.js handle a new message");
+      console.log(request);
+      console.log(sender);
+      // sendResponse("bar");
 
+      if (request.intent == "import") {
+        console.log("START IMPORT");
+        console.log($("#AppInfoAppNameInputBox"));
+        console.log($("#AppInfoAppIntroduceInputBox"));
+        console.log($("#AppInfoAppBriefInputBox"));
+        // injectScript(chrome.extension.getURL('js/injectScript.js'), 'body');
 
+        $("#AppInfoAppNameInputBox").val(request.name);
+        $("#AppInfoAppIntroduceInputBox").val(request.description);
+        $("#AppInfoAppBriefInputBox").val(request.brief); //.val("name")
 
+        $("#AppInfoAppBriefInputBox").val("request.brief"); //.val("name")
 
-$(document).ready(function () {
+        var event = new Event("change");
 
+        var element1 = document.getElementById("AppInfoAppNameInputBox");
+        var element2 = document.getElementById("AppInfoAppIntroduceInputBox");
+        var element3 = document.getElementById("AppInfoAppBriefInputBox");
+        element1.dispatchEvent(event);
+        element2.dispatchEvent(event);
+        element3.dispatchEvent(event);
 
+        sendResponse("done");
+      }
 
-    chrome.runtime.onMessage.addListener(
-        function (request, sender, sendResponse) {
-            console.log("content script.js handle a new message")
-            console.log(request);
-            console.log(sender);
-            // sendResponse("bar");
+      if (request.intent == "inject") {
+        console.log("START inject");
+        injectScript(chrome.extension.getURL("js/injectScript.js"), "body");
+      }
 
-            if (request.intent == "import") {
-                console.log("START IMPORT");
-                console.log($('#AppInfoAppNameInputBox'));
-                console.log($('#AppInfoAppIntroduceInputBox'));
-                console.log($('#AppInfoAppBriefInputBox'));
-                // injectScript(chrome.extension.getURL('js/injectScript.js'), 'body');
-
-                $('#AppInfoAppNameInputBox').val(request.name);
-                $('#AppInfoAppIntroduceInputBox').val(request.description);
-                $('#AppInfoAppBriefInputBox').val(request.brief); //.val("name")
-            }
-
-            if (request.intent == "inject") {
-                console.log("START inject");
-                injectScript(chrome.extension.getURL('js/injectScript.js'), 'body');
-
-
-            }
-
-
-            if (request.intent == "getlang") {
-                console.log("START getlang");
-                sendResponse(getElementByXpath('/html/body/main/section/div[1]/div[4]/div[3]/div[1]/div/div/div[2]/div[1]/div/div/div[1]/span/span[2]').innerText)
-
-            }
-
-            if (request.intent == "nextlang") {
-                console.log("START nextlang");
-                $('.btn.btn-default.form-control.ui-select-toggle')[0].click();
-                $('.ui-select-bootstrap .ui-select-choices-row.active>span').parent().next().click();
-                sendResponse($('.ui-select-bootstrap .ui-select-choices-row.active>span').parent().next().length);
-            }
-
-            if (request.intent == 'selectSupportedLang') {
-                console.log("START selectSupportedLang")
-                var supportedLanguage = request.lang;
-
-                if (document.getElementById("AppInfoManageLanguageButton")) {
-
-
-                    document.getElementById("AppInfoManageLanguageButton").click();
-
-                    setTimeout(() => {
-                        console.log("first timeout ")
-                        var langBtn = $('.ngdialog div.dialog-content.pt-2 .list-container label')
-
-                        langBtn.each((id, element) => {
-                            console.log(element.innerText);
-                            if (element.innerText in supportedLanguage) {
-                                $(element).click()
-                            }
-                        });
-                    }, 1000)
-
-                    setTimeout(() => {
-                        console.log("second timeout ");
-                        console.log($('.ngdialog .dialog-footer .btn-primary'));
-                        $('.ngdialog .dialog-footer .btn-primary')[0].click();
-                    }, 3000)
-                }
-            }
-
+      if (request.intent == "getlang") {
+        console.log("START getlang");
+        var lang = getElementByXpath(
+          "/html/body/main/section/div[1]/div[4]/div[3]/div[1]/div/div/div[2]/div[1]/div/div/div[1]/span/span[2]"
+        );
+        if (lang) {
+          sendResponse(lang.innerText);
         }
-    );
+      }
 
-    console.log("Script loaded")
-
-
-
-
-    window.addEventListener("message", function (event) {
-        this.console.log("event");
-        this.console.log(event.data.text);
-        // We only accept messages from ourselves
-        if (event.source != window)
-            return;
-
-        if (event.data.type && (event.data.type == "FROM_PAGE")) {
-            console.log("Content script received message: " + event.data.text);
+      if (request.intent == "nextlang") {
+        console.log("START nextlang");
+        var btnSelectLang = $(
+          ".btn.btn-default.form-control.ui-select-toggle:first"
+        );
+        if (btnSelectLang) {
+          btnSelectLang.click();
+          var isNext = !!$(
+            ".ui-select-bootstrap .ui-select-choices-row.active>span"
+          )
+            .parent()
+            .next().length;
+          $(".ui-select-bootstrap .ui-select-choices-row.active>span:first")
+            .parent()
+            .next()
+            .click();
+          sendResponse(isNext);
         }
+      }
+
+      if (request.intent == "selectSupportedLang") {
+        console.log("START selectSupportedLang");
+        var supportedLanguage = request.lang;
+
+        if (document.getElementById("AppInfoManageLanguageButton")) {
+          document.getElementById("AppInfoManageLanguageButton").click();
+
+          setTimeout(() => {
+            console.log("first timeout ");
+            var langBtn = $(
+              ".ngdialog div.dialog-content.pt-2 .list-container label"
+            );
+
+            langBtn.each((id, element) => {
+              console.log(element.innerText);
+              if (element.innerText in supportedLanguage) {
+                $(element).click();
+              }
+            });
+          }, 1000);
+          setTimeout(() => {
+            console.log("second timeout ");
+            console.log($(".ngdialog .dialog-footer .btn-primary"));
+            $(".ngdialog .dialog-footer .btn-primary")[0].click();
+          }, 3000);
+
+          //   var promiss = new Promise(function(resolve, reject) {
+
+          //   });
+          //   promiss.then(sendResponse("done"));
+          sendResponse("done");
+        }
+      }
     });
 
-    document.addEventListener('RW759_connectExtension', function (e) {
-        // e.detail contains the transferred data (can be anything, ranging
-        // from JavaScript objects to strings).
-        // Do something, for example:
-        alert(e.detail);
+    console.log("Script loaded");
+
+    window.addEventListener("message", function(event) {
+      this.console.log("event");
+      this.console.log(event.data.text);
+      // We only accept messages from ourselves
+      if (event.source != window) return;
+
+      if (event.data.type && event.data.type == "FROM_PAGE") {
+        console.log("Content script received message: " + event.data.text);
+      }
     });
 
+    document.addEventListener("RW759_connectExtension", function(e) {
+      // e.detail contains the transferred data (can be anything, ranging
+      // from JavaScript objects to strings).
+      // Do something, for example:
+      alert(e.detail);
+    });
 
     // var port = chrome.runtime.connect({name: "knockknock"});
     // port.postMessage({joke: "Knock knock"});
@@ -126,25 +154,25 @@ $(document).ready(function () {
     //     port.postMessage({answer: "Madame... Bovary"});
     // });
 
-    chrome.runtime.onConnect.addListener(function (port) {
-        console.assert(port.name == "knockknock");
-        port.onMessage.addListener(function (msg) {
-            if (msg.joke == "Knock knock")
-                port.postMessage({
-                    question: "Who's there?"
-                });
-            else if (msg.answer == "Madame")
-                port.postMessage({
-                    question: "Madame who?"
-                });
-            else if (msg.answer == "Madame... Bovary")
-                port.postMessage({
-                    question: "I don't get it."
-                });
-        });
+    chrome.runtime.onConnect.addListener(function(port) {
+      console.assert(port.name == "knockknock");
+      port.onMessage.addListener(function(msg) {
+        if (msg.joke == "Knock knock")
+          port.postMessage({
+            question: "Who's there?"
+          });
+        else if (msg.answer == "Madame")
+          port.postMessage({
+            question: "Madame who?"
+          });
+        else if (msg.answer == "Madame... Bovary")
+          port.postMessage({
+            question: "I don't get it."
+          });
+      });
     });
-})
-
+  }
+});
 
 // chrome.runtime.onMessage.addListener (
 //     function (request, sender, sendResponse) {
@@ -160,7 +188,6 @@ $(document).ready(function () {
 //                         ;
 //                     });
 //                 });
-
 
 //             })
 //         }
--- js/app.js
+++ js/app.js
@@ -1,347 +1,385 @@
-var name = localStorage.getItem('name');
-var desc = localStorage.getItem('desc');
-var brief = localStorage.getItem('brief');
-$("#inputName").val(name)
-$("#inputIntroduction").val(desc)
-$("#inputBriefIntroduction").val(brief)
+var name = localStorage.getItem("name");
+var desc = localStorage.getItem("desc");
+var brief = localStorage.getItem("brief");
+$("#inputName").val(name);
+$("#inputIntroduction").val(desc);
+$("#inputBriefIntroduction").val(brief);
 
-$("#inputName").change(function () {
+$("#inputName").change(function() {
   var selected = $("#inputName", this);
   var valueSelected = this.value;
-  console.log(valueSelected)
-  localStorage.setItem('name', valueSelected);
+  console.log(valueSelected);
+  localStorage.setItem("name", valueSelected);
 });
 
-
-$("#inputIntroduction").change(function () {
+$("#inputIntroduction").change(function() {
   var selected = $("#inputIntroduction", this);
   var valueSelected = this.value;
-  console.log(valueSelected)
-  localStorage.setItem('desc', valueSelected);
+  console.log(valueSelected);
+  localStorage.setItem("desc", valueSelected);
 });
 
-
-$("#inputBriefIntroduction").change(function () {
+$("#inputBriefIntroduction").change(function() {
   var selected = $("#inputBriefIntroduction", this);
   var valueSelected = this.value;
-  console.log(valueSelected)
-  localStorage.setItem('brief', valueSelected);
+  console.log(valueSelected);
+  localStorage.setItem("brief", valueSelected);
 });
 
 var supportedLanguage = {
-
-  'Afrikaans': 'af',
-  'Albanian': 'sq',
-  'Amharic': 'am',
-  'Arabic': 'ar',
-  'Armenian': 'hy',
-  'Azerbaijani': 'az',
-  'Basque': 'eu',
-  'Belarusian': 'be',
-  'Bengali': 'bn',
-  'Bosnian': 'bs',
-  'Bulgarian': 'bg',
-  'Catalan': 'ca',
-  'Cebuano': 'ceb',
-  'Chinese (Simplified)': 'zh-CN',
-  'Chinese (Traditional)': 'zh-TW',
-  'Corsican': 'co',
-  'Croatian': 'hr',
-  'Czech': 'cs',
-  'Danish': 'da',
-  'Dutch': 'nl',
-  'English': 'en',
-  'English (US)': 'en',
-  'English (UK)': 'en',
-  'Esperanto': 'eo',
-  'Estonian': 'et',
-  'Finnish': 'fi',
-  'French': 'fr',
-  'Frisian': 'fy',
-  'Galician': 'gl',
-  'Georgian': 'ka',
-  'German': 'de',
-  'Greek': 'el',
-  'Gujarati': 'gu',
-  'Haitian Creole': 'ht',
-  'Hausa': 'ha',
-  'Hawaiian': 'haw',
-  'Hebrew': 'he',
-  'Hindi': 'hi',
-  'Hmong': 'hmn',
-  'Hungarian': 'hu',
-  'Icelandic': 'is',
-  'Igbo': 'ig',
-  'Indonesian': 'id',
-  'Irish': 'ga',
-  'Italian': 'it',
-  'Japanese': 'ja',
-  'Javanese': 'jw',
-  'Kannada': 'kn',
-  'Kazakh': 'kk',
-  'Khmer': 'km',
-  'Korean': 'ko',
-  'Kurdish': 'ku',
-  'Kyrgyz': 'ky',
-  'Lao': 'lo',
-  'Latin': 'la',
-  'Latvian': 'lv',
-  'Lithuanian': 'lt',
-  'Luxembourgish': 'lb',
-  'Macedonian': 'mk',
-  'Malagasy': 'mg',
-  'Malay': 'ms',
-  'Malayalam': 'ml',
-  'Maltese': 'mt',
-  'Maori': 'mi',
-  'Marathi': 'mr',
-  'Mongolian': 'mn',
-  'Myanmar (Burmese)': 'my',
-  'Nepali': 'ne',
-  'Norwegian': 'no',
-  'Nyanja (Chichewa)': 'ny',
-  'Pashto': 'ps',
-  'Persian': 'fa',
-  'Polish': 'pl',
-  'Portuguese (Brazil)': 'pt',
-  'Punjabi': 'pa',
-  'Romanian': 'ro',
-  'Russian': 'ru',
-  'Samoan': 'sm',
-  'Scots Gaelic': 'gd',
-  'Serbian': 'sr',
-  'Sesotho': 'st',
-  'Shona': 'sn',
-  'Sindhi': 'sd',
-  'Sinhala': 'si',
-  'Slovak': 'sk',
-  'Slovenian': 'sl',
-  'Somali': 'so',
-  'Spanish': 'es',
-  'Sundanese': 'su',
-  'Swahili': 'sw',
-  'Swedish': 'sv',
-  'Tagalog (Filipino)': 'tl',
-  'Tajik': 'tg',
-  'Tamil': 'ta',
-  'Telugu': 'te',
-  'Thai': 'th',
-  'Turkish': 'tr',
-  'Ukrainian': 'uk',
-  'Urdu': 'ur',
-  'Uzbek': 'uz',
-  'Vietnamese': 'vi',
-  'Welsh': 'cy',
-  'Xhosa': 'xh',
-  'Yiddish': 'yi',
-  'Yoruba': 'yo',
-  'Zulu': 'zu',
-
-}
+  Afrikaans: "af",
+  Albanian: "sq",
+  Amharic: "am",
+  Arabic: "ar",
+  Armenian: "hy",
+  Azerbaijani: "az",
+  Basque: "eu",
+  Belarusian: "be",
+  Bengali: "bn",
+  Bosnian: "bs",
+  Bulgarian: "bg",
+  Catalan: "ca",
+  Cebuano: "ceb",
+  "Chinese (Simplified)": "zh-CN",
+  "Chinese (Traditional)": "zh-TW",
+  Corsican: "co",
+  Croatian: "hr",
+  Czech: "cs",
+  Danish: "da",
+  Dutch: "nl",
+  English: "en",
+  "English (US)": "en",
+  "English (UK)": "en",
+  Esperanto: "eo",
+  Estonian: "et",
+  Finnish: "fi",
+  French: "fr",
+  Frisian: "fy",
+  Galician: "gl",
+  Georgian: "ka",
+  German: "de",
+  Greek: "el",
+  Gujarati: "gu",
+  "Haitian Creole": "ht",
+  Hausa: "ha",
+  Hawaiian: "haw",
+  Hebrew: "he",
+  Hindi: "hi",
+  Hmong: "hmn",
+  Hungarian: "hu",
+  Icelandic: "is",
+  Igbo: "ig",
+  Indonesian: "id",
+  Irish: "ga",
+  Italian: "it",
+  Japanese: "ja",
+  Javanese: "jw",
+  Kannada: "kn",
+  Kazakh: "kk",
+  Khmer: "km",
+  Korean: "ko",
+  Kurdish: "ku",
+  Kyrgyz: "ky",
+  Lao: "lo",
+  Latin: "la",
+  Latvian: "lv",
+  Lithuanian: "lt",
+  Luxembourgish: "lb",
+  Macedonian: "mk",
+  Malagasy: "mg",
+  Malay: "ms",
+  Malayalam: "ml",
+  Maltese: "mt",
+  Maori: "mi",
+  Marathi: "mr",
+  Mongolian: "mn",
+  "Myanmar (Burmese)": "my",
+  Nepali: "ne",
+  Norwegian: "no",
+  "Nyanja (Chichewa)": "ny",
+  Pashto: "ps",
+  Persian: "fa",
+  Polish: "pl",
+  "Portuguese (Brazil)": "pt",
+  Punjabi: "pa",
+  Romanian: "ro",
+  Russian: "ru",
+  Samoan: "sm",
+  "Scots Gaelic": "gd",
+  Serbian: "sr",
+  Sesotho: "st",
+  Shona: "sn",
+  Sindhi: "sd",
+  Sinhala: "si",
+  Slovak: "sk",
+  Slovenian: "sl",
+  Somali: "so",
+  Spanish: "es",
+  Sundanese: "su",
+  Swahili: "sw",
+  Swedish: "sv",
+  "Tagalog (Filipino)": "tl",
+  Tajik: "tg",
+  Tamil: "ta",
+  Telugu: "te",
+  Thai: "th",
+  Turkish: "tr",
+  Ukrainian: "uk",
+  Urdu: "ur",
+  Uzbek: "uz",
+  Vietnamese: "vi",
+  Welsh: "cy",
+  Xhosa: "xh",
+  Yiddish: "yi",
+  Yoruba: "yo",
+  Zulu: "zu"
+};
 var supportedLanguage2 = {
-  'Amharic': 'am',
-  'Arabic': 'ar',
-  'Basque': 'eu',
-  'Bulgarian': 'bg',
-  'Catalan': 'ca',
-  'Chinese (PRC)': 'zh-CN',
-  'Croatian': 'hr',
-  'Czech': 'cs',
-  'Danish': 'da',
-  'Dutch': 'nl',
-  'Estonian': 'et',
-  'English (US)': 'en',
-  'English (UK)': 'en',
-  'Filipino': 'tl',
-  'Finnish': 'fi',
-  'French': 'fr',
-  'Galician': 'gl',
-  'Georgian': 'ka',
-  'German': 'de',
-  'Greek': 'el',
-  'Gujarati': 'gu',
-  'Hebrew': 'he',
-  'Hindi': 'hi',
-  'Hungarian': 'hu',
-  'Indonesian': 'id',
-  'Japanese': 'ja',
-  'Javanese': 'jw',
-  'Kannada': 'kn',
-  'Kazakh': 'kk',
-  'Khmer': 'km',
-  'Korean (South Korea)': 'ko',
-  'Lao': 'lo',
-  'Latvian': 'lv',
-  'Lithuanian': 'lt',
-  'Macedonian': 'mk',
-  'Malay': 'ms',
-  'Malayalam': 'ml',
-  'Maori': 'mi',
-  'Marathi': 'mr',
-  'Mongolian Cyrillic': 'mn',
-  'Nepali': 'ne',
-  'Norwegian': 'no',
-  'Persian': 'fa',
-  'Polish': 'pl',
-  'Portuguese (Brazil)': 'pt',
-  'Punjabi': 'pa',
-  'Romanian': 'ro',
-  'Russian': 'ru',
-  'Serbian': 'sr',
-  'Sinhala': 'si',
-  'Slovak': 'sk',
-  'Slovenian': 'sl',
-  'Spanish (Latin America)': 'es',
-  'Spanish (Spain)': 'es',
-  'Swedish': 'sv',
-  'Tamil': 'ta',
-  'Telugu': 'te',
-  'Thai': 'th',
-  'Traditional Chinese (Taiwan, China)': 'zh-TW',
-  'Traditional Chinese (Hong Kong, China)': 'zh-TW',
-  'Turkish': 'tr',
-  'Vietnamese': 'vi',
-  'Ukrainian': 'uk',
-  'Urdu': 'ur',
-  'Uzbek': 'uz',
+  Amharic: "am",
+  Arabic: "ar",
+  Basque: "eu",
+  Bulgarian: "bg",
+  Catalan: "ca",
+  "Chinese (PRC)": "zh-CN",
+  Croatian: "hr",
+  Czech: "cs",
+  Danish: "da",
+  Dutch: "nl",
+  Estonian: "et",
+  "English (US)": "en",
+  "English (UK)": "en",
+  Filipino: "tl",
+  Finnish: "fi",
+  French: "fr",
+  Galician: "gl",
+  Georgian: "ka",
+  German: "de",
+  Greek: "el",
+  Gujarati: "gu",
+  Hebrew: "he",
+  Hindi: "hi",
+  Hungarian: "hu",
+  Indonesian: "id",
+  Japanese: "ja",
+  Javanese: "jw",
+  Kannada: "kn",
+  Kazakh: "kk",
+  Khmer: "km",
+  "Korean (South Korea)": "ko",
+  Lao: "lo",
+  Latvian: "lv",
+  Lithuanian: "lt",
+  Macedonian: "mk",
+  Malay: "ms",
+  Malayalam: "ml",
+  Maori: "mi",
+  Marathi: "mr",
+  "Mongolian Cyrillic": "mn",
+  Nepali: "ne",
+  Norwegian: "no",
+  Persian: "fa",
+  Polish: "pl",
+  "Portuguese (Brazil)": "pt",
+  Punjabi: "pa",
+  Romanian: "ro",
+  Russian: "ru",
+  Serbian: "sr",
+  Sinhala: "si",
+  Slovak: "sk",
+  Slovenian: "sl",
+  "Spanish (Latin America)": "es",
+  "Spanish (Spain)": "es",
+  Swedish: "sv",
+  Tamil: "ta",
+  Telugu: "te",
+  Thai: "th",
+  "Traditional Chinese (Taiwan, China)": "zh-TW",
+  "Traditional Chinese (Hong Kong, China)": "zh-TW",
+  Turkish: "tr",
+  Vietnamese: "vi",
+  Ukrainian: "uk",
+  Urdu: "ur",
+  Uzbek: "uz"
+};
 
-}
-
-$(document).ready(function () {
-
-
+$(document).ready(function() {
   function translate(text, targetLang) {
     var settings = {
-      "async": false,
-      "crossDomain": true,
-      "url": "https://google-translate1.p.rapidapi.com/language/translate/v2",
-      "method": "POST",
-      "headers": {
+      async: false,
+      crossDomain: true,
+      url: "https://google-translate1.p.rapidapi.com/language/translate/v2",
+      method: "POST",
+      headers: {
         "x-rapidapi-host": "google-translate1.p.rapidapi.com",
         "x-rapidapi-key": "407e38fc61msh9aa66a9888202ecp1eb0f4jsn21e3b5654c7a",
         "content-type": "application/x-www-form-urlencoded"
       },
-      "data": {
-        "source": "en",
-        "q": text,
-        "target": targetLang
+      data: {
+        source: "en",
+        q: text,
+        target: targetLang
       }
-    }
+    };
 
-    return res = $.ajax(settings).responseJSON.data.translations[0].translatedText
+    return (res = $.ajax(settings).responseJSON.data.translations[0]
+      .translatedText);
   }
 
   function popupToForm() {
-
-    var lang = $("#inputSupported").val()
-    var message = {
-      intent: "import",
-      name: translate($("#inputName")[0].value, lang),
-      description: translate($("#inputIntroduction")[0].value, lang),
-      brief: translate($("#inputBriefIntroduction")[0].value, lang)
-    }
-    chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
-        console.log(response);
-        return response
-      });
-    });
-
+    return new Promise(function(resolve, reject) {
+      var lang = $("#inputSupported").val();
+      var message = {
+        intent: "import",
+        name: translate($("#inputName")[0].value, lang),
+        description: translate($("#inputIntroduction")[0].value, lang),
+        brief: translate($("#inputBriefIntroduction")[0].value, lang)
+      };
+      chrome.tabs.query(
+        {
+          active: true,
+          currentWindow: true
+        },
+        function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
+            console.log(`popupToForm resolving : ${response}`);
+            resolve(response);
+          });
+        }
+      );
+    }); // end promisse
   }
 
   function nextLang() {
-
-    var message = {
-      intent: "nextlang"
-    }
-    chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
-        console.log(response);
-        return response
-
-      });
-    });
-
+    return new Promise(function(resolve, reject) {
+      var message = {
+        intent: "nextlang"
+      };
+      chrome.tabs.query(
+        {
+          active: true,
+          currentWindow: true
+        },
+        function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
+            console.log(`nextLang getting : ${response}`);
+            resolve(response);
+          });
+        }
+      );
+    }); // end promisse
   }
 
   function selectSupportedLang() {
-    var message = {
-      intent: "selectSupportedLang",
-      lang: supportedLanguage2
-    }
-    chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
-        console.log(response);
-        return response
-
-      });
-    });
-
+    return new Promise(function(resolve, reject) {
+      var message = {
+        intent: "selectSupportedLang",
+        lang: supportedLanguage2
+      };
+      chrome.tabs.query(
+        {
+          active: true,
+          currentWindow: true
+        },
+        function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
+            console.log(`selectSupportedLang get : ${response}`);
+            resolve(response);
+          });
+        }
+      );
+    }); // end promisse
   }
 
-  $('#btnImport').on('click', popupToForm);
+  function getlang() {
+    return new Promise(function(resolve, reject) {
+      var message = {
+        intent: "getlang"
+      };
+      chrome.tabs.query(
+        {
+          active: true,
+          currentWindow: true
+        },
+        function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
+            console.log(`getlang get : ${response}`);
+            $("#inputLang").val(response);
+            $("#inputSupported").val(supportedLanguage2[response]);
+            console.log(
+              `getlang resolving : ${!!supportedLanguage2[response]}`
+            );
+            resolve(!!supportedLanguage2[response]);
+          });
+        }
+      );
+    });
+  }
 
-  $('#nextlang').on('click', nextLang);
+  $("#btnImport").on("click", popupToForm);
 
-  $('#selectLang').on('click', selectSupportedLang);
+  $("#nextlang").on("click", nextLang);
 
-  $('#inject').on('click', function () {
+  $("#selectLang").on("click", selectSupportedLang);
+
+  $("#inject").on("click", function() {
     var message = {
       intent: "inject"
+    };
+    chrome.tabs.query(
+      {
+        active: true,
+        currentWindow: true
+      },
+      function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
+          console.log(response);
+        });
+      }
+    );
+  });
+
+  $("#translateAll").on("click", async function() {
+    // var message = {
+    //   intent: "translateAll"
+    // }
+    // chrome.tabs.query({
+    //   active: true,
+    //   currentWindow: true
+    // }, function (tabs) {
+    //   chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
+    //     console.log(response);
+    //   });
+    // });
+    function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
     }
-    chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
-        console.log(response);
-      });
-    });
 
-  })
+    // getlang();
+    // await sleep(200);
+    // popupToForm();
 
-  $('#translateAll').on('click', function () {
-    var message = {
-      intent: "translateAll"
+    // while (nextLang()) {
+    //   await sleep(500);
+    //   getlang();
+    //   await sleep(500);
+    //   popupToForm();
+    // }
+    var end = false;
+    while (!end) {
+      await getlang();
+      await popupToForm();
+      // await sleep(500);
+
+      var isNext = await nextLang();
+      if (!isNext) {
+        end = true;
+      }
     }
-    chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
-        console.log(response);
-      });
-    });
 
-
-  })
-  $('#getlang').on('click', function () {
-    var message = {
-      intent: "getlang"
-    }
-    chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
-        console.log(response);
-        $("#inputLang").val(response)
-        $("#inputSupported").val(supportedLanguage2[response])
-      });
-    });
-  })
-
+    console.log("translate is over");
+  });
+  // $("#getlang").on("click", getlang); // end promisse
 });
-
 
 // var port = chrome.tabs.connect({name: "knockknock"});
 // port.postMessage({joke: "Knock knock"});
@@ -351,7 +389,6 @@ $(document).ready(function () {
 //   else if (msg.question == "Madame who?")
 //     port.postMessage({answer: "Madame... Bovary"});
 // });
-
 
 // chrome.runtime.onMessageExternal.addListener(
 //     function(request, sender, sendResponse) {
@@ -368,7 +405,6 @@ $(document).ready(function () {
 //           });
 
 //     });
-
 
 // chrome.runtime.onMessage.addListener(
 //     function(request, sender, sendResponse) {
